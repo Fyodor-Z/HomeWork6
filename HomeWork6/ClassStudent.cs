e97b06733e685a695ada45745d2a1cb4b3215f9d
fyodor <fyodor.zakharov@gmail.com>
2021-03-12 01:35:10 +0300

    Student final

---
+++ HomeWork6/ClassStudent.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork6
+{
+    public class Student
+
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int[] Marks { get; set; }
+
+
+
+        public Student(string firstName = "Безымянный", string lastName = "Безфамильный")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Marks = new int[] { 0 };
+
+        }
+
+        public void MarksOutput()
+        {
+            Console.WriteLine($"Студент {FirstName} {LastName}");
+            Console.Write($"Оценки: ");
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                Console.Write($"{Marks[i]} ");
+            }
+            Console.WriteLine();
+        }
+        public float AverageScore(bool output=false) //подсчет среднего балла с возможностью вывода
+        {
+            float sum = 0;
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                sum += Marks[i];
+            }
+            if (output)
+            {
+                Console.Write($"Студент {FirstName} {LastName} средний балл {(sum / Marks.Length): #.##}");
+            }
+            return sum / Marks.Length;
+        }
+
+
+    }
+
+    public class StudentsGroup
+    {
+        public string GroupName { get; set; }
+        public List<Student> Students { get; set; }
+
+        public StudentsGroup(string name = "НоваяГруппа")
+        {
+            GroupName = name;
+            Students = new List<Student>(1);
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="student">Студент, которого переводим</param>
+        /// <param name="Group">Группа, в которую нужно перевести</param>
+        /// <returns>true, если перевод успешный и false, если нет</returns>
+        public bool Transfer(Student student, StudentsGroup Group)
+        {
+
+            if (Group != null)//если группа для перевода существует
+            {
+                bool result = Students.Remove(student);
+                if (result)
+                {
+                    Group.AddStudent(student);
+                }
+            }
+
+            return false;
+        }
+
+        public void MarksOutput()
+        {
+            Console.WriteLine($"Группа {GroupName} ");
+            foreach (var stdnt in Students)
+            {
+                stdnt.MarksOutput();
+            }
+        }
+
+        public void AverageOutput()
+        {
+            Console.WriteLine($"Группа {GroupName} ");
+            foreach (var stdnt in Students)
+            {
+                stdnt.AverageScore(true);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -6,7 +6,60 @@ namespace HomeWork6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //создадим объекты и заполним данные о нескольких студентах
+            Student student1 = new Student("Иван", "Петров");
+            student1.Marks = new int[] { 7,8,5,9,8,7};
+            Student student2 = new Student("Петр", "Иванов");
+            student2.Marks = new int[] { 4, 8, 7 };
+            Student student3 = new Student("Андей", "Сергеев");
+            student3.Marks = new int[] { 5, 6, 8, 7 };
+            Student student4 = new Student("Максим", "Андреев");
+            student4.Marks = new int[] { 8, 8, 6, 6, 8, 7 };
+            Student student5 = new Student("Дарья", "Максимова");
+            student5.Marks = new int[] { 6, 8, 7, 7 };
+
+            StudentsGroup group1 = new StudentsGroup("СШ-2021");
+            StudentsGroup group2 = new StudentsGroup("ФШ-2021");
+
+            //распредилим студентов по группам
+            group1.AddStudent(student1);
+            group1.AddStudent(student2);
+
+            group2.AddStudent(student3);
+            group2.AddStudent(student4);
+            group2.AddStudent(student5);
+
+            //выводим оценки и средний балл
+            group1.MarksOutput();
+            Console.WriteLine();
+            group1.AverageOutput();
+
+            Console.WriteLine();
+
+            group2.MarksOutput();
+            Console.WriteLine();
+            group2.AverageOutput();
+
+            //переводим студента из группы ФШ-2021 в СШ-2021
+            group2.Transfer(student5, group1);
+
+            Console.WriteLine();
+            Console.WriteLine("После перевода студента");
+            Console.WriteLine();
+
+            //выводим оценки и средний балл
+            group1.MarksOutput();
+            Console.WriteLine();
+            group1.AverageOutput();
+
+            Console.WriteLine();
+
+            group2.MarksOutput();
+            Console.WriteLine();
+            group2.AverageOutput();
+
+
+            Console.ReadLine();
         }
     }
 }
